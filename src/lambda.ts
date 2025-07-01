7a7e6f2a906132a5ea4baf402d3ffa8b45757bb3
Artem Katnov <akatnov@upteam.com>
2025-07-02 00:46:19 +0200

    add logic to work with lambda from event bridge

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,9 +1,13 @@
 import serverlessExpress from "@vendia/serverless-express";
 import { createApp } from "./server";
+import service from "@service";
 
 let cachedServer: ReturnType<typeof serverlessExpress> | null = null;
 
 exports.handler = async (event: any, context: any) => {
+  if (event.source === "aws.events") {
+    return await wakeUp();
+  }
   if (!cachedServer) {
     const app = await createApp();
     cachedServer = serverlessExpress({ app });
@@ -11,3 +15,8 @@ exports.handler = async (event: any, context: any) => {
 
   return cachedServer!(event, context);
 };
+
+async function wakeUp() {
+  await createApp();
+  await service.wakeUpChat();
+}
