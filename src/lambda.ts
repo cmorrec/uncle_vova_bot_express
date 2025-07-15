428b2baa13732acdfc3836d8f11afb6ded15c062
Artem Katnov <akatnov@upteam.com>
2025-07-15 20:22:47 +0200

    update lambda trigger check

--- src/lambda.ts
+++ src/lambda.ts
@@ -5,9 +5,19 @@ import service from "@service";
 let cachedServer: ReturnType<typeof serverlessExpress> | null = null;
 
 exports.handler = async (event: any, context: any) => {
-  if (event.source === "aws.events") {
+  const isEventBridgeScheduled =
+    event?.source === "aws.events" &&
+    event?.["detail-type"] === "Scheduled Event";
+  if (isEventBridgeScheduled) {
     console.log("Received event:", JSON.stringify(event));
-    return await wakeUp();
+    await wakeUp();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "wakeUp() executed from EventBridge Scheduler",
+      }),
+    };
   }
   if (!cachedServer) {
     const app = await createApp();
